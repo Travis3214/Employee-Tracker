b5174e794b023910f3c6de2bf5fff0d62754f184
Travis <travis3214@yahoo.com>
2023-08-03 23:16:41 -0500

    add db for the employees in schema along with some tables

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS employeesDB;
+CREATE DATABASE employeesDB;
+
+USE employees_DB;
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT NULL,
+  manager_id INT NULL,
+);
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(45) NULL,
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(75) NULL,
+  salary DECIMAL(10.3) NULL,
+  department_id INT NULL,
+);
